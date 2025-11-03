292290a1f852ee708d1ee3ee6222cfc115ebca9c
Aditya Sen <adityasen120@gmail.com>
2025-11-03 14:40:21 -0500

    rendered sphere

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,29 @@
 #include <iostream>
 
 
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 origin_sphere = center - r.origin();
+
+    //quadratic formula
+    auto a = dot(r.direction(), r.direction());
+
+    auto b = -2.0 * dot(r.direction(), origin_sphere);
+    
+    auto c = dot(origin_sphere, origin_sphere) - (radius * radius);
+
+    auto discriminant = (b * b) - (4 * a * c);
+
+    if (discriminant >= 0) {
+        return true;
+    }
+
+    return false;
+}
+
 color ray_color(const ray& r) {
+    if (hit_sphere(point3(0, 0, -1), 0.5, r)) return color(1, 0, 0);
+
+
     vec3 unit_direction = unit_vector(r.direction());
 
     auto a = 0.5 * (unit_direction.y() + 1.0);
